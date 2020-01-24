ba744425f9f313866f73c52d2f383282dbc5c0f2
Bhupendra Aole <aole@users.noreply.github.com>
2020-01-24 11:01:16 -0500

    Gradient Control
    Closes Issue #3

--- gradienteditor.py
+++ gradienteditor.py
@@ -7,6 +7,11 @@ Bhupendra Aole
 import wx
 from wx.lib.agw.cubecolourdialog import CubeColourDialog
 
+class GradientStop(wx.GraphicsGradientStop):
+    def __init__(self, color, pos):
+        super().__init__(color, pos)
+        self.active = True
+        
 class GradientControl(wx.Window):
     PADDING = 10
     PADDING2 = PADDING*2
@@ -32,43 +37,63 @@ class GradientControl(wx.Window):
         
         if not stops:
             stops = wx.GraphicsGradientStops(wx.BLACK, wx.WHITE)
-            stops.Add(wx.GraphicsGradientStop(wx.BLACK, 0))
-            stops.Add(wx.GraphicsGradientStop(wx.WHITE, 1))
+            stops.Add(GradientStop(wx.BLACK, 0))
+            stops.Add(GradientStop(wx.WHITE, 1))
         self.stops = stops
         
-        self.markers = [wx.GraphicsGradientStop(stops.StartColour, 0), wx.GraphicsGradientStop(stops.EndColour, 1)]
+        self.markers = [GradientStop(stops.StartColour, 0), GradientStop(stops.EndColour, 1)]
         for stop in range(1, stops.Count-1):
             self.markers.append(stops[stop])
             
+        self.alphabg = wx.Bitmap("alphabg.png")
+
     def UpdateStops(self):
-        self.stops = wx.GraphicsGradientStops(self.markers[0].Colour, self.markers[1].Colour)
+        minm = (2, None)
+        maxm = (-1, None)
         for marker in self.markers[2:]:
+            if isinstance(marker, GradientStop) and not marker.active:
+                continue
+            if marker.Position < minm[0]:
+                minm = (marker.Position, marker.Colour)
+            if marker.Position > maxm[0]:
+                maxm = (marker.Position, marker.Colour)
+            
+        self.stops = wx.GraphicsGradientStops(minm[1], maxm[1])
+        for marker in self.markers[2:]:
+            if isinstance(marker, GradientStop) and not marker.active:
+                continue
             self.stops.Add(marker)
             
     def MarkerUnderLocation(self, mx, my):
-        for idx, marker in enumerate(self.markers[2:]):
+        for marker in self.markers[2:]:
             x = (self.width-GradientControl.PADDING2) * marker.Position + GradientControl.PADDING
             y = 75
-            r = wx.Rect(x-3, y+8, 6, 8)
+            r = wx.Rect(x-4, y-6, 8, 25)
             if r.Contains(mx, my):
-                return marker, idx
+                return marker
                 
-        return None, -1
+        return None
         
     def SetMarkerLocation(self, marker, x, y):
-        px = max(GradientControl.PADDING, min(x, self.width-GradientControl.PADDING)) - GradientControl.PADDING
-        px /= self.width-GradientControl.PADDING2
-        marker.SetPosition(px)
+        if x<0 or x>self.width or y<0 or y>self.height and len(self.markers)>4:
+            marker.active = False
+        else:
+            marker.active = True
+            px = max(GradientControl.PADDING, min(x, self.width-GradientControl.PADDING)) - GradientControl.PADDING
+            px /= self.width-GradientControl.PADDING2
+            marker.SetPosition(px)
         
-    def DrawMarker(self, gc, color, x, y, highlight=False):
+    def DrawMarker(self, gc, x, y, highlight=False):
         if highlight:
             gc.SetPen(wx.Pen(wx.Colour(250,250,250), 3))
         else:
             gc.SetPen(wx.Pen(wx.Colour(50,50,50), 2))
         gc.StrokeLine(x, y-4, x, y+4)
         
-        gc.SetPen(wx.Pen(color, 2))
         gc.DrawRoundedRectangle(x-3, y+8, 6, 8, 3)
+        
+    def RemoveUnactiveMarkers(self):
+        self.markers = [x for x in self.markers if not isinstance(x, GradientStop) or x.active]
         
     def OnPaint(self, e):
         dc = wx.AutoBufferedPaintDC(self)
@@ -78,17 +103,21 @@ class GradientControl(wx.Window):
         
         gc = wx.GraphicsContext.Create(dc)
 
+        gc.DrawBitmap(self.alphabg, GradientControl.PADDING, GradientControl.PADDING, self.width-GradientControl.PADDING2, 45)
+        
         gc.SetPen(wx.Pen(wx.BLACK, 5))
         gc.StrokeLine(GradientControl.PADDING, 75, self.width-GradientControl.PADDING, 75)
         
         for marker in self.markers[2:]:
+            if isinstance(marker, GradientStop) and not marker.active:
+                continue
             x = (self.width-GradientControl.PADDING2)*marker.Position + GradientControl.PADDING
-            self.DrawMarker(gc, marker.Colour, x, 75, marker==self.dragging)
+            self.DrawMarker(gc, x, 75, marker==self.dragging)
         
         brush = gc.CreateLinearGradientBrush(GradientControl.PADDING, 0, self.width-GradientControl.PADDING, 0, self.stops)
         gc.SetBrush(brush)
         gc.SetPen(wx.NullPen)
-        gc.DrawRoundedRectangle(GradientControl.PADDING, GradientControl.PADDING, self.width-GradientControl.PADDING2, 45, 5)
+        gc.DrawRectangle(GradientControl.PADDING, GradientControl.PADDING, self.width-GradientControl.PADDING2, 45)
         
     def GetStops(self):
         return self.stops
@@ -105,7 +134,7 @@ class GradientControl(wx.Window):
         x, y = e.GetPosition()
         self.prevx, self.prevy = x, y
         
-        self.dragging, idx = self.MarkerUnderLocation(x, y)
+        self.dragging = self.MarkerUnderLocation(x, y)
         self.left_down = True
         
         if not self.HasCapture():
@@ -123,11 +152,12 @@ class GradientControl(wx.Window):
             self.ReleaseMouse()
             
         self.Refresh()
+        self.RemoveUnactiveMarkers()
         
     def OnLeftDClick(self, e):
         x, y = e.GetPosition()
         if self.prevx==x and self.prevy==y:
-            marker, idx = self.MarkerUnderLocation(x, y)
+            marker = self.MarkerUnderLocation(x, y)
             if marker:
                 data = wx.ColourData()
                 data.SetColour(marker.Colour)
@@ -135,8 +165,29 @@ class GradientControl(wx.Window):
                 if dlg.ShowModal() == wx.ID_OK:
                     color = dlg.GetColourData().GetColour()
                     marker.SetColour(color)
-                    self.markers[idx].SetColour(color)
-                        
+            else: # add new marker
+                if x <= self.width-GradientControl.PADDING and x>=GradientControl.PADDING:
+                    pos = (x-GradientControl.PADDING) / (self.width-GradientControl.PADDING2)
+                    # find markers before and after pos to interpolate
+                    bef = None
+                    aft = None
+                    for marker in self.markers:
+                        if marker.Position<=pos and (not bef or marker.Position>bef.Position):
+                            bef = marker
+                        if marker.Position>=pos and (not aft or marker.Position<aft.Position):
+                            aft = marker
+                            
+                    # interpolate before and after color
+                    w = aft.Position - bef.Position
+                    ip = (pos-bef.Position)/w
+                    icr = bef.Colour.red+(aft.Colour.red - bef.Colour.red)*ip
+                    icg = bef.Colour.green+(aft.Colour.green - bef.Colour.green)*ip
+                    icb = bef.Colour.blue+(aft.Colour.blue - bef.Colour.blue)*ip
+                    ica = bef.Colour.alpha+(aft.Colour.alpha - bef.Colour.alpha)*ip
+                    stop = GradientStop(wx.Colour(icr, icg, icb, ica), pos)
+                    
+                    self.markers.append(stop)
+                    
         self.UpdateStops()
         self.Refresh()
         
@@ -162,5 +213,10 @@ class GradientEditor(wx.Dialog):
         
 if __name__ == '__main__':
     app = wx.App()
-    print(GradientEditor().ShowModal())
-    
+    ge = GradientEditor()
+    ge.ShowModal()
+    stops = ge.GetStops()
+    for i in range(stops.GetCount()):
+        stop = stops[i]
+        print(str(i+1), round(stop.Position,1), stop.Colour)
+        
